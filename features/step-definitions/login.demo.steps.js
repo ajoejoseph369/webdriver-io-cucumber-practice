9f4a2583e22e0b75458db0fcd4fb56cdc4c03103
Ajoe Joseph <ajoe.joseph@people10.com>
2024-04-22 17:28:27 +0530

    Main Commit

---
+++ features/step-definitions/login.demo.steps.js
@@ -0,0 +1,20 @@
+// const LoginPage = require('../pages/login.page')
+
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+
+Given(/^user is on login page$/, async () => {
+    await browser.url('https://the-internet.herokuapp.com/login');
+});
+
+When(/^user enters (.*) and (.*)$/, async (username,password) => {
+    await $('#username').setValue(username);
+    await $('#password').setValue(password);
+});
+
+When(/^clicks on login button$/, async () => {
+    await $('button[type="submit"]').click();
+});
+
+Then(/^this (.*) is displayed$/, async (message) => {
+    await expect($('#flash')).toHaveText(expect.stringContaining(message));
+});
